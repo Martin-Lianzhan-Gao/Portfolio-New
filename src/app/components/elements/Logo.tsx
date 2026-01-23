527ff162196981bbbdc5156595eb91241be3992f
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-01-24 01:11:59 +1000

    Create interactive svg logo

---
+++ src/app/components/elements/Logo.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+const Logo = () => {
+    return (
+        <div>
+            <svg width="47" height="21" viewBox="0 0 47 21" fill="none">
+                <path d="M28.6149 18.3548H43.6149C20.8649 -7.42402 3.36493 -0.549673 1.61493 18.3548C21.2466 22.8024 22.6149 13.4806 24.1149 4.49556" stroke="black" strokeWidth="3" strokeLinecap="square" />
+            </svg>
+        </div>
+    )
+}
+
+export default Logo;
