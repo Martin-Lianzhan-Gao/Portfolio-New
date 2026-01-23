54af9e809732e279904ee3843c84476a792809c0
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-01-24 01:14:04 +1000

    Add menu icon and replace icon by new interactive icon

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,60 +1,44 @@
 'use client'
-import Logo from "./Logo";
+
+import Logo from './elements/Logo'
 
 const Header = () => {
 
     return (
-        <div className="w-full h-auto flex flex-row items-center justify-between pt-6">
-            <div className="w-fit ml-8 font-roboto-condensed flex flex-row items-center">
-                <Logo />
-                <p className="font-bold text-[28px] ml-4">MARTIN /
-                    <span className="font-medium text-gray-400"> PORTFOLIO</span>
+        <div className="absolute top-0 w-full h-auto flex flex-row items-center justify-between mt-6 z-10">
+            <div className="w-auto h-auto ml-12 flex flex-row items-center">
+                <div className='mr-2'>
+                    <Logo />
+                </div>
+                <p className='font-montserrat font-medium text-3xl'>
+                    Martin Gao
                 </p>
             </div>
-            <button className="mr-8">
-                <svg
-                    viewBox="0 0 40 40"
-                    width={58}
-                    height={58}
-                >
-                    <circle
-                        cx={20}
-                        cy={20}
-                        r={18}
-                        fill="none"
-                    />  
-                        <line
-                            id="line-top"
-                            x1={8}
-                            y1={15}
-                            x2={32}
-                            y2={15}
-                            stroke="#000"
-                            strokeWidth={1.4}
-                            strokeLinecap="round"
+            {/* Header Menu */}
+            <div className='w-auto h-auto mr-12'>
+                <svg width={40} height={40} viewBox='0 0 40 15' fill='none'>
+                    <line
+                        id='line-top'
+                        x1="2"
+                        y1="3"
+                        x2="40"
+                        y2="3"
+                        stroke="black"
+                        strokeWidth="3"
+                        strokeLinecap="square"
                     />
                     <line
-                        id="line-middle"
-                        x1={8}
-                        y1={20}
-                        x2={32}
-                        y2={20}
-                        stroke="#000"
-                        strokeWidth={1.4}
-                        strokeLinecap="round"
-                    />
-                    <line
-                        id="line-bottom"
-                        x1={8}
-                        y1={25}
-                        x2={32}
-                        y2={25}
-                        stroke="#000"
-                        strokeWidth={1.4}
-                        strokeLinecap="round"
+                        id='line-bottom'
+                        x1="2"
+                        y1="12"
+                        x2="40"
+                        y2="12"
+                        stroke="black"
+                        strokeWidth="3"
+                        strokeLinecap="square"
                     />
                 </svg>
-            </button>
+            </div>
         </div>
     )
 }
