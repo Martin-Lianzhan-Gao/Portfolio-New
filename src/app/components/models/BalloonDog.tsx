4c66b3e5ccf55c5e1c62f79c689656c00ccbb754
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-02-09 23:30:32 +1000

    Updated react and nextjs version

--- src/app/components/models/BalloonDog.tsx
+++ src/app/components/models/BalloonDog.tsx
@@ -5,8 +5,7 @@ import Spline from "@splinetool/react-spline";
 export default function BalloonDog() {
     return (
         <div className="bg-white w-full h-full">
-            <Spline
-                scene="/models/baloon_dog.spline" />
+            <Spline scene="/models/baloon_dog.spline" />
         </div>
     );
 }
