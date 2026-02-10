a06dabc5d0e469a9b5f04a61dba8454443162c45
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-02-10 11:51:44 +1000

    Update title flow layout

--- src/app/Pages/Intro.tsx
+++ src/app/Pages/Intro.tsx
@@ -2,10 +2,12 @@
 
 const Intro = () => {
     return (
-        <div className="flex flex-col md:flex-row w-full h-[100dvh] overflow-x-hidden ">
-            <div className="h-[20dvh]"></div>
-            <div className="w-full h-[50dvh] flex flex-row items-center">
-                <h1 className="ml-6 font-inria-sans text-9xl font-bold max-w-full break-words">PORT<br />FOL<br />IO.</h1>
+        <div className="flex flex-col w-full h-[100dvh] overflow-x-hidden md:flex-row">
+            <div className="w-full h-[70dvh]">
+                <div className="h-[20dvh] w-full"></div>
+                <div className="flex flex-row items-center h-[50dvh] max-w-full @container">
+                    <h1 className="ml-6 mr-6 font-inria-sans text-[30cqw] font-bold break-all">PORTFOLIO.</h1>
+                </div>
             </div>
             <div className="font-medium h-[30dvh] flex flex-col justify-center items-end mr-6">
                 <p>AESTHETIC.</p>
