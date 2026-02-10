586d74533a59abaac387a0e3bb46860c36b23f10
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-02-10 12:15:21 +1000

    Update slogan along with font size adjustment

--- src/app/Pages/Intro.tsx
+++ src/app/Pages/Intro.tsx
@@ -6,13 +6,23 @@ const Intro = () => {
             <div className="w-full h-[70dvh]">
                 <div className="h-[20dvh] w-full"></div>
                 <div className="flex flex-row items-center h-[50dvh] max-w-full">
-                    <h1 className="ml-6 mr-6 font-inria-sans text-9xl font-bold break-all">PORTFOLIO.</h1>
+                    <div className="ml-6 mr-6 font-inria-sans text-9xl font-bold">
+                        <h1>PORT</h1>
+                        <h1>FOL</h1>
+                        <h1>IO.</h1>
+                    </div>
                 </div>
             </div>
-            <div className="font-medium h-[30dvh] flex flex-col justify-center items-end mr-6">
-                <p>AESTHETIC.</p>
-                <p>EXPERIENCE.</p>
-                <p>INNOVATION.</p>
+            <div className="font-semibold h-[30dvh] flex flex-col justify-center">
+                <div className="flex flex-col items-end mr-6">
+                    <p>DEV WITH</p>
+                    <p>AESTHETIC,</p>
+                    <p>PASSION,</p>
+                    <p>INNOVATION.</p>
+                </div>
+                {/* <div className="flex flex-row justify-start ml-6">
+                    <p>(SCROLL DOWN)</p>
+                </div> */}
             </div>
         </div>
     )
