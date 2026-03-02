acdd3310ea3434be6f4a3be3cf90047920fdf19e
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-03-02 23:56:38 +1000

    Add self description section

---
+++ src/app/Pages/Description.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+const Description = () => {
+    return (
+        <div className="w-full @container">
+            <div className="text-[3cqw] font-inter uppercase font-light">
+                <p>
+                    A Computer Science graduate dedicated to building high-performance web interfaces.
+                    Merging technical precision with user-centered design to create scalable applications. Driven by curiosity and a constant pursuit of innovation.
+                </p>
+                <p>
+                    I specialize in the full-stack development of modern web applications, with a strong focus on front-end performance and user experience. My technical expertise spans across the entire development lifecycle, from initial concept and UI/UX design to deployment and maintenance.
+                </p>
+
+                <p>Scroll down to learn more about my work and experience.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Description;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import Intro from "./Pages/Intro";
+import Description from "./Pages/Description";
 
 export default function Page() {
 
     return (
         <div className="font-inter">
             <Intro />
+            <Description />
         </div>
     );
 }
