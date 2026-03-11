1d1969750be114aef2afd36c9b399c9f6aea4135
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-03-11 17:04:05 +1000

    build(git): integrate commitlint with husky to enforce conventional commits

---
+++ commitlint.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'build']
+        ],
+        'subject-case': [0, 'never'],
+        'type-case': [2, 'always', 'lower-case'],
+    },
+};
