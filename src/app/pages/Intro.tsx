82e0c7a8594dcab376d6cae34d85deea61953db2
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-04-10 00:36:47 +1000

    chore(id): add id for smooth scroll naviation

--- src/app/pages/Intro.tsx
+++ src/app/pages/Intro.tsx
@@ -104,7 +104,7 @@ const Intro = () => {
     });
 
     return (
-        <div className="sticky top-0 z-0 w-full h-[100dvh] overflow-hidden bg-white">
+        <div id="top" className="sticky top-0 z-0 w-full h-[100dvh] overflow-hidden bg-white">
             {/* Particle Sphere Background */}
             <div
                 ref={particleSphereContainerRef}
