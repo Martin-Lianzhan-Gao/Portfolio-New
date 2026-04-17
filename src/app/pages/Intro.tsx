4abe3a984ccccf2c6f6137aefd58c64e39830653
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-04-17 15:23:56 +1000

    style(z-index): adjust index

--- src/app/pages/Intro.tsx
+++ src/app/pages/Intro.tsx
@@ -104,7 +104,7 @@ const Intro = () => {
     });
 
     return (
-        <div id="top" className="sticky top-0 z-0 w-full h-[100dvh] overflow-hidden bg-white">
+        <div id="top" className="sticky top-0 z-10 w-full h-[100dvh] overflow-hidden bg-white">
             {/* Particle Sphere Background */}
             <div
                 ref={particleSphereContainerRef}
