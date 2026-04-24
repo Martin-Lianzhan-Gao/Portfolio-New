9b67e7764451d9b8b29565997688b52df11551b9
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2026-04-24 17:18:14 +1000

    chore: adjust stroke width to fit header design

--- src/app/components/ui/Logo.tsx
+++ src/app/components/ui/Logo.tsx
@@ -14,7 +14,7 @@ const Logo = forwardRef<SVGPathElement, LogoProps>(({ color = 'currentColor' }, 
                     ref={ref}
                     d="M28.6149 18.3548H43.6149C20.8649 -7.42402 3.36493 -0.549673 1.61493 18.3548C21.2466 22.8024 22.6149 13.4806 24.1149 4.49556"
                     stroke={color}
-                    strokeWidth="3"
+                    strokeWidth="2.5"
                     strokeLinecap="square"
                 />
             </svg>
